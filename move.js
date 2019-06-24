20dab7429e068b41d0e01d95c76b3c4530a350cd
Molsagari92 <gabi.sajti@gmail.com>
2019-06-24 15:19:04 +0200

    movement modified

--- move.js
+++ move.js
@@ -1,17 +1,38 @@
-const move = (arr) => {
+
+const move = (arr, where) => {
+  let readline = require('readline-sync');
   let a = readline.keyIn('Press a key! ');
-  if (a = 'w') {
-
-  } else if (a = 's') {
-
-  } else if (a = 'a') {
-
-  } else if (a = 'd') {
-
+  let place = where.split('');
+  let x = place[0];
+  let y = place[1];
+  if (a === 'w') {
+    if (x > 1) {
+      x = x - 1;
+    } else {
+      console.log('Invalid movement!');
+    }
+  } else if (a === 's') {
+    if (x < arr.length - 1) {
+      x = x + 1;
+    } else {
+      console.log('Invalid movement!');
+    }
+  } else if (a === 'a') {
+    if (y < arr[x].length - 1) {
+      y = y + 1;
+    } else {
+      console.log('Invalid movement!');
+    }
+  } else if (a === 'd') {
+    if (y > 1) {
+      y = y - 1;
+    } else {
+      console.log('Invalid movement!');
+    }
   }
 };
 
 /* w=up;
 s=down;
 a=left;
-d=right,*/
+d=right, */
